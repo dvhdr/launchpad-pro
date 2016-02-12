6edc776b3d0a33c1b59ed29d66570cc8b5bc74e2
Dave Hodder <dave.hodder@focusrite.com>
2016-02-12 17:39:46 +0000

    default app sends to DIN MIDI

--- src/app.c
+++ src/app.c
@@ -51,7 +51,7 @@ void app_surface_event(u8 type, u8 index, u8 value)
 		{
 			// example - light / extinguish pad LEDs, send MIDI
 			hal_plot_led(TYPEPAD, index, value, value, value);
-			hal_send_midi(USBSTANDALONE, NOTEON | 0, index, value);
+			hal_send_midi(DINMIDI, NOTEON | 0, index, value);
 		}
 		break;
 
